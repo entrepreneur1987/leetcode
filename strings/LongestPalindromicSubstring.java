0ba863f52fa1f52aa24ec2c8399782e4613ef74c
entrepreneur1987 <luyao.li1987@gmail.com>
2015-05-06 09:09:22 -0700

    Update LongestPalindromicSubstring.java

--- strings/LongestPalindromicSubstring.java
+++ strings/LongestPalindromicSubstring.java
@@ -20,4 +20,30 @@ public class Solution {
         }
         return s.substring(maxStart, maxStart+maxLen);
     }
+    
+    // span from middle
+    public String longestPalindrome(String s) {
+        String longest = "";
+        for(int i=0; i<2*s.length()-1; i++){
+            int left = i/2;
+            int right = i/2;
+            if(i%2==1)
+                right++;
+            String t = longestPalindrome(s, left, right);
+            if(t.length() > longest.length()){
+                longest = t;
+            }
+        }
+        return longest;
+    }
+    
+    private String longestPalindrome(String s, int left, int right){
+        while(left >=0 && right<s.length()){
+            if(s.charAt(left) != s.charAt(right))
+                break;
+            left--;
+            right++;
+        }
+        return s.substring(left+1, right);
+    }
 }
